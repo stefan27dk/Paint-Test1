e39d7358edec1ec414214d12d8d4ab0444c77e95
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-08-18 16:32:57 +0200

    AutoResize Right Tool Panel

--- Paint-Test1/Form1.Designer.cs
+++ Paint-Test1/Form1.Designer.cs
@@ -28,36 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.right_tool_panel = new System.Windows.Forms.Panel();
+            this.top_tool_panel = new System.Windows.Forms.Panel();
+            this.left_tool_panel = new System.Windows.Forms.Panel();
+            this.left_mesuring_pictureBox = new System.Windows.Forms.PictureBox();
+            this.top_mesuring_pictureBox = new System.Windows.Forms.PictureBox();
+            this.main_holder_panel = new System.Windows.Forms.Panel();
+            this.workplace_pictureBox = new System.Windows.Forms.PictureBox();
+            this.autoResize_Right_Tool_Panel_checkBox = new System.Windows.Forms.CheckBox();
+            this.right_tool_panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.left_mesuring_pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.top_mesuring_pictureBox)).BeginInit();
+            this.main_holder_panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.workplace_pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel1
+            // right_tool_panel
             // 
-            this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Location = new System.Drawing.Point(12, 27);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1333, 665);
-            this.panel1.TabIndex = 0;
-            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
-            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.right_tool_panel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.right_tool_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(74)))), ((int)(((byte)(74)))));
+            this.right_tool_panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.right_tool_panel.Controls.Add(this.autoResize_Right_Tool_Panel_checkBox);
+            this.right_tool_panel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.right_tool_panel.Location = new System.Drawing.Point(1122, 69);
+            this.right_tool_panel.Name = "right_tool_panel";
+            this.right_tool_panel.Size = new System.Drawing.Size(235, 689);
+            this.right_tool_panel.TabIndex = 2;
+            // 
+            // top_tool_panel
+            // 
+            this.top_tool_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.top_tool_panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.top_tool_panel.Location = new System.Drawing.Point(0, 0);
+            this.top_tool_panel.Name = "top_tool_panel";
+            this.top_tool_panel.Size = new System.Drawing.Size(1357, 69);
+            this.top_tool_panel.TabIndex = 3;
+            // 
+            // left_tool_panel
+            // 
+            this.left_tool_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(54)))), ((int)(((byte)(94)))));
+            this.left_tool_panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.left_tool_panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.left_tool_panel.Location = new System.Drawing.Point(0, 69);
+            this.left_tool_panel.Name = "left_tool_panel";
+            this.left_tool_panel.Size = new System.Drawing.Size(121, 689);
+            this.left_tool_panel.TabIndex = 4;
+            // 
+            // left_mesuring_pictureBox
+            // 
+            this.left_mesuring_pictureBox.BackColor = System.Drawing.Color.Navy;
+            this.left_mesuring_pictureBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.left_mesuring_pictureBox.Location = new System.Drawing.Point(121, 96);
+            this.left_mesuring_pictureBox.Name = "left_mesuring_pictureBox";
+            this.left_mesuring_pictureBox.Size = new System.Drawing.Size(37, 662);
+            this.left_mesuring_pictureBox.TabIndex = 5;
+            this.left_mesuring_pictureBox.TabStop = false;
+            // 
+            // top_mesuring_pictureBox
+            // 
+            this.top_mesuring_pictureBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(84)))), ((int)(((byte)(84)))));
+            this.top_mesuring_pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.top_mesuring_pictureBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.top_mesuring_pictureBox.Location = new System.Drawing.Point(121, 69);
+            this.top_mesuring_pictureBox.Name = "top_mesuring_pictureBox";
+            this.top_mesuring_pictureBox.Size = new System.Drawing.Size(1001, 27);
+            this.top_mesuring_pictureBox.TabIndex = 6;
+            this.top_mesuring_pictureBox.TabStop = false;
+            this.top_mesuring_pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.top_mesuring_pictureBox_Paint);
+            // 
+            // main_holder_panel
+            // 
+            this.main_holder_panel.BackColor = System.Drawing.Color.White;
+            this.main_holder_panel.Controls.Add(this.workplace_pictureBox);
+            this.main_holder_panel.Controls.Add(this.left_mesuring_pictureBox);
+            this.main_holder_panel.Controls.Add(this.top_mesuring_pictureBox);
+            this.main_holder_panel.Controls.Add(this.right_tool_panel);
+            this.main_holder_panel.Controls.Add(this.left_tool_panel);
+            this.main_holder_panel.Controls.Add(this.top_tool_panel);
+            this.main_holder_panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.main_holder_panel.Location = new System.Drawing.Point(0, 0);
+            this.main_holder_panel.Name = "main_holder_panel";
+            this.main_holder_panel.Size = new System.Drawing.Size(1357, 758);
+            this.main_holder_panel.TabIndex = 7;
+            // 
+            // workplace_pictureBox
+            // 
+            this.workplace_pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.workplace_pictureBox.Location = new System.Drawing.Point(158, 96);
+            this.workplace_pictureBox.Name = "workplace_pictureBox";
+            this.workplace_pictureBox.Size = new System.Drawing.Size(964, 662);
+            this.workplace_pictureBox.TabIndex = 7;
+            this.workplace_pictureBox.TabStop = false;
+            this.workplace_pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.workplace_pictureBox_MouseDown);
+            this.workplace_pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.workplace_pictureBox_MouseMove);
+            // 
+            // autoResize_Right_Tool_Panel_checkBox
+            // 
+            this.autoResize_Right_Tool_Panel_checkBox.AutoSize = true;
+            this.autoResize_Right_Tool_Panel_checkBox.Checked = true;
+            this.autoResize_Right_Tool_Panel_checkBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.autoResize_Right_Tool_Panel_checkBox.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.autoResize_Right_Tool_Panel_checkBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.autoResize_Right_Tool_Panel_checkBox.Location = new System.Drawing.Point(18, 5);
+            this.autoResize_Right_Tool_Panel_checkBox.Name = "autoResize_Right_Tool_Panel_checkBox";
+            this.autoResize_Right_Tool_Panel_checkBox.Size = new System.Drawing.Size(102, 17);
+            this.autoResize_Right_Tool_Panel_checkBox.TabIndex = 0;
+            this.autoResize_Right_Tool_Panel_checkBox.Text = "Auto Resize";
+            this.autoResize_Right_Tool_Panel_checkBox.UseVisualStyleBackColor = true;
+            this.autoResize_Right_Tool_Panel_checkBox.CheckedChanged += new System.EventHandler(this.autoResize_Right_Tool_Panel_checkBox_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(1357, 704);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(1357, 758);
+            this.Controls.Add(this.main_holder_panel);
+            this.MinimumSize = new System.Drawing.Size(450, 500);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.right_tool_panel.ResumeLayout(false);
+            this.right_tool_panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.left_mesuring_pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.top_mesuring_pictureBox)).EndInit();
+            this.main_holder_panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.workplace_pictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel right_tool_panel;
+        private System.Windows.Forms.Panel top_tool_panel;
+        private System.Windows.Forms.Panel left_tool_panel;
+        private System.Windows.Forms.PictureBox left_mesuring_pictureBox;
+        private System.Windows.Forms.PictureBox top_mesuring_pictureBox;
+        private System.Windows.Forms.Panel main_holder_panel;
+        private System.Windows.Forms.PictureBox workplace_pictureBox;
+        private System.Windows.Forms.CheckBox autoResize_Right_Tool_Panel_checkBox;
     }
 }
 
--- Paint-Test1/Form1.cs
+++ Paint-Test1/Form1.cs
@@ -12,25 +12,39 @@ namespace Paint_Test1
 {
     public partial class Form1 : Form
     {
-
+        // Paint
         public Point current = new Point();
         public Point old = new Point();
         public Pen pen1 = new Pen(Color.Red, 5);
         public Graphics g;
         public bool isMouseMoving = false;
-        
 
 
+        // Mesure
+        public Point ruller_Location;
+
+
+        // Right Tool Panel Size
+        Size right_tool_panel_Last_size;
+        bool right_panel_resized = false;
+
+
+        // Auto Risize Right Tool Panel
+        bool auto_Resize_Right_Tool_panel = true;
+           
 
         // Initialize
         public Form1()
         {
             InitializeComponent();
-            g = panel1.CreateGraphics();
+            // Paint
+            g = workplace_pictureBox.CreateGraphics();
             pen1.SetLineCap(System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.DashCap.Round);
+           
         }
 
 
+        
 
 
 
@@ -42,42 +56,151 @@ namespace Paint_Test1
 
 
 
+      
+            
+
+          
 
 
-        // Mouse Move - Panel
-        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        // Top mesuring
+        private void top_mesuring_pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            
+
+           Graphics g = e.Graphics;
+
+              
+            for (int x = 50; x < top_mesuring_pictureBox.Width; x += 100)  // Start at 50px. and draw line every 100px till it reaches the Picturebox end "Lenght"
+            {
+                g.DrawLine(new Pen(new SolidBrush(Color.Red), 3.0f), new Point(x,0), new Point(x,20)); // Pen is drawing the outter and Brush is filling the inner
+            }
+
+
+
+            //// ----------------
+            //Graphics g = e.Graphics;
+            //for (int x = 30; x < top_mesuring_pictureBox.Width; x += 100)
+            //{
+            //    g.DrawLine(new Pen(new SolidBrush(Color.Red), 3.0f), new Point(x, 0), new Point(x, 20));
+            //}
+            //for (int x = 30; x < top_mesuring_pictureBox.Width; x += 50)
+            //{
+            //    g.DrawLine(new Pen(new SolidBrush(Color.Blue), 2.0f), new Point(x, 0), new Point(x, 10));
+            //}
+            //for (int x = 30; x < top_mesuring_pictureBox.Width; x += 10)
+            //{
+            //    g.DrawLine(new Pen(new SolidBrush(Color.Red), 1.0f), new Point(x, 0), new Point(x, 5));
+            //}
+
+            //g.DrawLine(new Pen(new SolidBrush(Color.Red), 2.0f), new Point(ruller_Location.X, 0), new Point(ruller_Location.X, 10));
+        }
+
+
+
+
+
+
+
+
+
+
+
+        // Mouse Down - WORKPLACE
+        private void workplace_pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (isMouseMoving == false)
+            {
+                g.DrawLine(pen1, e.Location, new Point(e.Location.X - 1, e.Location.Y - 1));
+            }
+
+            old = e.Location;
+        }
+
+
+
+
+
+
+
+
+
+        // Mouse Move - WORKPLACE
+        private void workplace_pictureBox_MouseMove(object sender, MouseEventArgs e)
         {
             isMouseMoving = true;
-
             if (e.Button == MouseButtons.Left)
             {
                 current = e.Location;
                 g.DrawLine(pen1, old, current);
                 old = current;
             }
-
             isMouseMoving = false;
-
         }
 
 
 
-     
-     
 
 
-
-        // Mouse Down
-        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        // Form Resize
+        private void Form1_Resize(object sender, EventArgs e)
         {
-            if(isMouseMoving == false)
-            {
-                g.DrawLine(pen1, e.Location, new Point(e.Location.X-10, e.Location.Y-10));
-            }
 
-            old = e.Location;
+
+            // Auto Resize Right Tool Panel:::::::::::::::::START::::::::::::::::::::::::
+            if(auto_Resize_Right_Tool_panel == true)
+            {
+
+                if(this.Width <= 800 && right_tool_panel.Width >= 100)
+                {
+                    right_tool_panel_Last_size = right_tool_panel.Size; // Remember last Size
+                    right_tool_panel.Width = 99;
+                    right_panel_resized = true;
+                }
+               
+                else if(this.Width >= 800 && right_panel_resized == true)
+                {
+                    right_tool_panel.Width = right_tool_panel_Last_size.Width;
+                    right_panel_resized = false;
+                }
+            }
+            // Auto Resize Right Tool Panel:::::::::::::::::END::::::::::::::::::::::::
+
+
+
         }
 
+
+
+        // AutoResize Checked Changed
+        private void autoResize_Right_Tool_Panel_checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            // Right Tool Panel - Toggle AutoResize:::::::::START::::::::::::
+            if(autoResize_Right_Tool_Panel_checkBox.Checked == true)
+            {
+                auto_Resize_Right_Tool_panel = true;
+            }
+            else  
+            {
+                auto_Resize_Right_Tool_panel = false;
+            }
+            // Right Tool Panel - Toggle AutoResize:::::::::END::::::::::::
+
+        }
+
+
+
+
+
+
+
+        // Follow Line
+
+        //        if(isMouseMoving == false)
+        //        {
+        //            g.DrawLine(pen1, old, e.Location);
+        //        }
+
+        //old = e.Location;
 
 
 
